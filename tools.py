8424b7ee30dfdfa6fa2668c89ac1876da6d9f851
davalb <david.albert@insa-rouen.fr>
2019-10-21 12:45:43 +0200

    Add data augmentation

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 import math
 
 def save_img(filename, np_array):
@@ -11,6 +12,15 @@ def save_img(filename, np_array):
         np_array: the image to save
     """
     cv2.imwrite("{}.png".format(filename), np_array)
+    
+def read_img(filename):
+    """Read an image from a filename
+    
+    Args:
+        filename: the file name
+        np_array: the image to save
+    """
+    return mpimg.imread(filename)
     
 def group_images(imgs, num_rows=2, num_columns=4):
     """Group several images into one image
@@ -43,8 +53,7 @@ def display(img):
     Args:
         img: the image to display
     """ 
-    im = np.uint8(img)
-    plt.imshow(im)
+    plt.imshow(img)
     plt.axis('off')
     plt.show()
 
@@ -84,4 +93,35 @@ def deprocess(np_array):
     Args:
         np_array: the array to process
     """
-    return np.int16(process(np_array, x_inf=-1., x_sup=1., y_inf=0., y_sup=255.))
+    return np.uint8(process(np_array, x_inf=-1., x_sup=1., y_inf=0., y_sup=255.))
+
+def make_noisy(img, crop_size=256, blur=6, std_bright=.2):
+    """Modify data (Data augmentation trick)
+     
+    Args:
+        img: image to process
+        crop_size: crop dim
+        blur: size of blurred kernel
+        std_bright: std deviation of Normal distribution (0 centered) to 
+    """
+    img_w,img_h = img.shape[:2]
+    if img_w>crop_size and img_h>crop_size:
+        # Cropping data randomly
+        new_h = int(np.ceil(np.random.uniform(1e-2, img_w-crop_size)))
+        new_w = int(np.ceil(np.random.uniform(1e-2, img_h-crop_size)))
+        new_img = img[new_h:new_h+crop_size, new_w:new_w+crop_size]
+    
+    # Changing brightness
+    bright = np.random.normal(0.,std_bright)    
+    new_img = new_img + bright
+    new_img[np.where(new_img>1.)] = 1.
+    new_img[np.where(new_img<-1.)] = -1.
+    
+    # Blurring randomly image from triangular distribution
+    blurred_size = np.random.randint(np.random.randint(blur)+1)
+    if blurred_size>0:
+        new_img = cv2.blur(new_img, (blurred_size,blurred_size))
+    
+    if np.random.random() > 0.5:
+        new_img = np.fliplr(new_img)    
+    return new_img
